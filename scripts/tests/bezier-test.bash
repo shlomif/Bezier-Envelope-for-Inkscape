df5c58376c1f30d28c460dd32f566c521d5a5cdd
Shlomi Fish <shlomif@shlomifish.org>
2020-07-25 13:43:55 +0300

    cleanup the script

--- scripts/tests/bezier-test.bash
+++ scripts/tests/bezier-test.bash
@@ -3,4 +3,9 @@ set -e -x
 test -e bezierenvelope.py
 test -e .git
 test -e scripts/tests/test2.svg
-PYTHONPATH=/usr/share/inkscape/extensions python3 bezierenvelope.py --id=path837 --id=path839 --output out.svg "`pwd`"/scripts/tests/test2.svg
+if test -f "out.svg"
+then
+    rm -f "out.svg"
+fi
+PYTHONPATH=/usr/share/inkscape/extensions python3 "`pwd`/bezierenvelope.py" --id=path839 --id=path837 --output "`pwd`/out.svg" "`pwd`"/scripts/tests/test2.svg
+inkscape "`pwd`/out.svg"
