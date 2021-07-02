85984f8e30d3837654ab92cafb734e3bef3924e2
Shlomi Fish <shlomif@shlomifish.org>
2021-07-02 11:45:56 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- scripts/tests/bezier-test.bash
+++ scripts/tests/bezier-test.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e -x
+src_path="`pwd`"
 test -e bezierenvelope.py
 test -e .git
 test -e scripts/tests/test2.svg
@@ -7,5 +8,5 @@ if test -f "out.svg"
 then
     rm -f "out.svg"
 fi
-PYTHONPATH=/usr/share/inkscape/extensions python3 "`pwd`/bezierenvelope.py" --id=path839 --id=path837 --output "`pwd`/out.svg" "`pwd`"/scripts/tests/test2.svg
-inkscape "`pwd`/out.svg"
+PYTHONPATH=/usr/share/inkscape/extensions python3 "${src_path}/bezierenvelope.py" --id=path839 --id=path837 --output "${src_path}/out.svg" "${src_path}"/scripts/tests/test2.svg
+inkscape "${src_path}/out.svg"
